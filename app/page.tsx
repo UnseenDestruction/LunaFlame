0a0e694e6331a4718859da5202151c00b306de7b
Geric Augusto <gericgwapo9@gmail.com>
2024-12-01 12:56:41 +0800

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,45 +2,70 @@
 
 import React from "react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+
+type Star = {
+  width: string;
+  height: string;
+  top: string;
+  left: string;
+  opacity: number;
+  animationDuration: string;
+};
 
 const Home = () => {
+
+  const [stars, setStars] = useState<Star[]>([]);
+
+  useEffect(() => {
+    const generateStars = () =>
+      Array.from({ length: 100 }).map(() => ({
+        width: `${Math.random() * 2 + 1}px`,
+        height: `${Math.random() * 2 + 1}px`,
+        top: `${Math.random() * 100}%`,
+        left: `${Math.random() * 100}%`,
+        opacity: Math.random() * 0.7 + 0.3,
+        animationDuration: `${Math.random() * 5 + 5}s`,
+      }));
+    setStars(generateStars());
+  }, []);
+
   return (
     <main className="bg-black">
-      <div className="w-[100vw] h-[120vh] md:w-[100vw] md:h-[120vh] overflow-hidden mb-0">
+      <div className="w-[100vw] h-[120vh] lg:w-[100vw] lg:h-[120vh] overflow-hidden mb-[100px] xxs:mb-[0px] lg:mb-[0px] ">
       <div className="absolute inset-0 bg-gradient-to-b  from-[#0D0D33] via-[#101052] to-black"/>
-    
-
       <div className="absolute inset-0 pointer-events-none">
-          {Array.from({ length: 100 }).map((_, i) => (
+      {stars.map((star, i) => (
             <div
               key={i}
               className="absolute bg-white rounded-full opacity-10"
               style={{
-                width: `${Math.random() * 2 + 1}px`,
-                height: `${Math.random() * 2 + 1}px`,
-                top: `${Math.random() * 100}%`,
-                left: `${Math.random() * 100}%`,
-                opacity: Math.random() * 0.7 + 0.3,
-                animation: `twinkle ${Math.random() * 5 + 5}s infinite`,
+                width: star.width,
+                height: star.height,
+                top: star.top,
+                left: star.left,
+                opacity: star.opacity,
+                animation: `twinkle ${star.animationDuration} infinite`,
               }}
             ></div>
           ))}
         </div>
-      <div className="absolute  z-50 md:top-[10px] md:left-[0px]">
+      <div className="absolute  z-50 lg:top-[10px] lg:left-[0px] left-[-10px] xxs:left-[20px] xs:left-[50px] ssm:left-[100px] md:left-[300px] sm:left-[150px] ">
         <Image 
                       src="/luna.svg"
                       alt="Luna"
                       width={600}
                       height={600}
-                      className="relative md:left-[73%] md:top-[-60px] top-[-30px] left-[20px] animate-pulse z-50 "
+                      className="relative lg:left-[73%] lg:top-[-60px] top-[-55px] left-[30px] animate-pulse z-50 w-[350px] h-[400px] lg:w-[600px] lg:h-[600px] sssm:w-[400px] sssm:h-[400px] ssm:w-[400px] sm:h-[400px] sm:top-[-35px] sm:w-[400px] ssm:h-[400px] sssm:top-[-35px] ssm:top-[-35px]"
                     />
 
-        <div className="bg-black rounded-full md:w-[500px] md:h-[500px] w-[250px] h-[250px] absolute top-[-140px] left-[80px] md:left-[490px] md:top-[-300px] shadow-[0_0_200px_60px_rgba(255,0,0,0.2)] "> 
+        <div className="bg-black rounded-full lg:w-[500px] lg:h-[500px] w-[250px] h-[250px] xs:h-[300px] xs:w-[300px] sssm:h-[300px] sssm:w-[300px] ssm:top-[-170px] ssm:h-[300px] ssm:w-[300px] sm:h-[300px] sm:w-[300px] sm:top-[-170px] sssm:top-[-170px] xs:top-[-170px] xs:left-[60px] absolute top-[-140px] md:top-[-120px] md:h-[250px] md:w-[250px] md:left-[100px] left-[80px] lg:left-[490px] lg:top-[-300px] shadow-[0_0_200px_60px_rgba(255,0,0,0.2)] " /> 
+        
         </div>
-        </div>
-      <div className="relative md:top-[-750px] top-[-450px] md:h-[220vh] h-[150vh]  right-[250px] md:right-[50px] z-50 w-[220vw] md:w-[100vw] ">
+      <div className="relative lg:top-[-750px] top-[-480px] lg:h-[220vh] h-[150vh]  right-[260px] lg:left-[-50px] z-50 w-[220vw] md:w-[100vw] lg:w-[100vw] xs:top-[-550px] xs:right-[260px] sssm:right-[360px] sssm:top-[-580px] ssm:right-[400px] ssm:top-[-780px] xxs:top-[-500px] xxs:right-[260px]   sm:top-[-1000px] sm:right-[550px] md:top-[-500px] md:left-[0px] ">
     <ol >
-  <li className="sun hidden md:visible"></li>
+  <li className="sun hidden lg:visible"></li>
   <li className="earth"></li>
   <li className="mars"></li>
   <li className="jupiter"></li>
@@ -53,18 +78,18 @@ const Home = () => {
 </ol>
 </div>
 </div>
-<div className="flex justify-start md:justify-center items-center flex-col h-[50vh] md:h-[100vh] relative top-[-100px] md:top-[0px] w-[100vw] ">
-{Array.from({ length: 100 }).map((_, i) => (
+<div className="flex justify-start lg:justify-center items-center flex-col h-[50vh] lg:h-[100vh] relative top-[-100px] lg:top-[0px] w-[100vw] ">
+{stars.map((star, i) => (
             <div
               key={i}
-              className="absolute bg-white rounded-full"
+              className="absolute bg-white rounded-full opacity-10"
               style={{
-                width: `${Math.random() * 2 + 1}px`,
-                height: `${Math.random() * 2 + 1}px`,
-                top: `${Math.random() * 100}%`,
-                left: `${Math.random() * 100}%`,
-                opacity: Math.random() * 0.7 + 0.3,
-                animation: `twinkle ${Math.random() * 5 + 5}s infinite`,
+                width: star.width,
+                height: star.height,
+                top: star.top,
+                left: star.left,
+                opacity: star.opacity,
+                animation: `twinkle ${star.animationDuration} infinite`,
               }}
             ></div>
           ))}
@@ -83,24 +108,24 @@ const Home = () => {
                             <div 
                             
                             className="bg-[#FC0160] h-[20px] w-[20px] rounded-full" />
-                     <a href="mailto:your-email@example.com">
-                     <button
-          className="
-            bg-[#FC0160] 
-            rounded-full 
-            p-3
-            px-8
-            flex 
-            items-center 
-            shadow-lg
-            text-black
-            font-bold
-          "
-          onClick={() => window.location.href = 'mailto:your-email@example.com'}
-        >
-          Join Waitlist
-        </button>
-        </a>
+                <a href="mailto:unseenworkmail@gmail.com?subject=Join%20Waitlist&body=I%20want%20to%20join 🥺">
+  <button
+    className="
+      bg-[#FC0160] 
+      rounded-full 
+      p-3
+      px-8
+      flex 
+      items-center 
+      shadow-lg
+      text-black
+      font-bold
+    "
+  >
+    Join Waitlist
+  </button>
+</a>
+
         
                         <div  className="bg-[#FC0160] h-[20px] w-[20px] rounded-full" />
         
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,60 +8,14 @@ export default {
   ],
   theme: {
     extend: {
-      animation: {
-        orbit1: 'spinOrbit1 4s linear infinite',
-        orbit2: 'spinOrbit2 14s linear infinite',
-        orbit3: 'spinOrbit3 18s linear infinite',
-        orbit4: 'spinOrbit4 22s linear infinite',
-        orbit5: 'spinOrbit5 26s linear infinite',
-        orbit6: 'spinOrbit6 30s linear infinite',
-      },
-      keyframes: {
-        spinOrbit1: {
-          '0%': { transform: ' translateX(0px) rotate(-20deg) ' },
-          '5%': { transform: 'translateX(-20px) translateY(20px) rotate(-20deg)' },
-          '10%': { transform: 'translateX(-40px) translateY(30px) rotate(-20deg)' },
-          '15%': { transform: 'translateX(-30px) translateY(40px) rotate(-20deg)' },
-          '20%': { transform: 'translateX(-40px) translateY(55px) rotate(-10deg)' },
-          '25%': { transform: 'translateX(-50px) translateY(66px) rotate(-10deg)' },
-          '30%': { transform: 'translateX(-70px) translateY(80px) rotate(-10deg)' },
-          '35%': { transform: 'translateX(-80px) translateY(90px) rotate(-5deg)' },
-          '40%': { transform: 'translateX(-90px) translateY(100px) rotate(-5deg)' },
-          '45%': { transform: 'translateX(-100px) translateY(110px) rotate(10deg)' },
-          '50%': { transform: 'translateX(-110px) translateY(110px) rotate(10deg)' },
-          '55%': { transform: 'translateX(-120px) translateY(110px) rotate(10deg)' },
-          '60%': { transform: 'translateX(-130px) translateY(120px) rotate(10deg)' },
-          '65%': { transform: 'translateX(-140px) translateY(120px) rotate(10deg)' },
-          '70%': { transform: 'translateX(-150px) translateY(130px) rotate(10deg)' },
-          '75%': { transform: 'translateX(-160px) translateY(135px) rotate(10deg)' },
-          '80%': { transform: 'translateX(-170px) translateY(135px) rotate(30deg)' },
-          '85%': { transform: 'translateX(-180px) translateY(135px) rotate(30deg)' },
-          '90%': { transform: 'translateX(-190px) translateY(148px) rotate(30deg)' },
-          '95%': { transform: 'translateX(-200px) translateY(148px) rotate(30deg)' },
-          '100%': { transform: 'translateX(-210px) translateY(148px) rotate(30deg)' },
-        },
-        spinOrbit2: {
-          '0%': { transform: 'rotate(0deg) translateX(150px) rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg) translateX(150px) rotate(-360deg)' },
-        },
-        spinOrbit3: {
-          '0%': { transform: 'rotate(0deg) translateX(200px) rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg) translateX(200px) rotate(-360deg)' },
-        },
-        spinOrbit4: {
-          '0%': { transform: 'rotate(0deg) translateX(250px) rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg) translateX(250px) rotate(-360deg)' },
-        },
-        spinOrbit5: {
-          '0%': { transform: 'rotate(0deg) translateX(300px) rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg) translateX(300px) rotate(-360deg)' },
-        },
-        spinOrbit6: {
-          '0%': { transform: 'rotate(0deg) translateX(350px) rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg) translateX(350px) rotate(-360deg)' },
-        },
+      screens: {
+        'xs': { 'min': '432px', 'max': '459px' },
+        'xxs':  { 'min': '400px', 'max': '431px' },
+        'sssm':  { 'min': '460px', 'max': '519px' },
+        'ssm':  { 'min': '520px', 'max': '639px' },
       },
     },
   },
+
   plugins: [],
 } satisfies Config;
